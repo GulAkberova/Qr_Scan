3faf53309b00efa69aa4b8677a0b3d6862c0af85
GulAkberova <kbrovagul0@gmail.com>
2024-05-20 15:56:26 +0400

    tt

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,8 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 import QrCreate from './components/QrCreate'
 import QrRead from './components/QrRead'
-import QrCameraScan from './components/QrCameraScan'
 
 function App() {
-  const [scanResult, setScanResult] = useState('');
-
-  const handleResult = (result, error) => {
-    if (result) {
-      setScanResult(result.text);
-    }
-    if (error) {
-      console.error(error);
-    }
-  };
 
 
   return (
@@ -27,11 +13,7 @@ function App() {
         <QrCreate/>
         <QrRead/>
         {/* <QrCameraScan/> */}
-        <div className="App">
-      <h1>QR Code Scanner</h1>
-      <QrCameraScan onResult={handleResult} />
-      <p>Scan Result: {scanResult}</p>
-    </div>
+     
       </div>
     </section>
     </>
