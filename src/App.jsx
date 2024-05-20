fde5a86d44562bb73c951c904646070a912dad9e
GulAkberova <kbrovagul0@gmail.com>
2024-05-20 15:44:29 +0400

    qr reader

--- src/App.jsx
+++ src/App.jsx
@@ -4,10 +4,20 @@ import viteLogo from '/vite.svg'
 import './App.css'
 import QrCreate from './components/QrCreate'
 import QrRead from './components/QrRead'
-// import QrCameraScan from './components/QrCameraScan'
+import QrCameraScan from './components/QrCameraScan'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [scanResult, setScanResult] = useState('');
+
+  const handleResult = (result, error) => {
+    if (result) {
+      setScanResult(result.text);
+    }
+    if (error) {
+      console.error(error);
+    }
+  };
+
 
   return (
     <>
@@ -17,6 +27,11 @@ function App() {
         <QrCreate/>
         <QrRead/>
         {/* <QrCameraScan/> */}
+        <div className="App">
+      <h1>QR Code Scanner</h1>
+      <QrCameraScan onResult={handleResult} />
+      <p>Scan Result: {scanResult}</p>
+    </div>
       </div>
     </section>
     </>
--- src/components/QrCameraScan.jsx
+++ src/components/QrCameraScan.jsx
@@ -1,31 +1,23 @@
-// import React, { useState } from 'react';
-// import QrReader from 'react-qr-reader';
+// QrCameraScan.jsx
+import React from 'react';
+import { QrReader } from 'react-qr-reader';
 
-// const QrCodeScanner = () => {
-//   const [qrCodeData, setQrCodeData] = useState('');
+const QrCameraScan = ({ onResult  }) => {
+  const previewStyle = {
+    height: 240,
+    width: 320,
+  };
 
-//   const handleScan = (data) => {
-//     if (data) {
-//       setQrCodeData(data);
-//     }
-//   };
+  return (
+    <div>
+      <QrReader
+        delay={300}
+        style={previewStyle}
+        onResult={onResult}
 
-//   const handleError = (err) => {
-//     console.error(err);
-//   };
+      />
+    </div>
+  );
+};
 
-//   return (
-//     <div>
-//       <h1>QR Kod Scanner</h1>
-//       <QrReader
-//         delay={300}
-//         onError={handleError}
-//         onScan={handleScan}
-//         style={{ width: '100%' }}
-//       />
-//       <p>QR Kod Məlumatı: {qrCodeData}</p>
-//     </div>
-//   );
-// };
-
-// export default QrCodeScanner;
+export default QrCameraScan;
